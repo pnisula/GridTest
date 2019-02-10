c33769d180ec2a30a02f97665194729ae8374969
Pekka Nisula <nisula.pekka@gmail.com>
2019-02-10 11:49:20 +0200

    Initial commit

---
+++ Source/GridTest/GridTestGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GridTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRIDTEST_API AGridTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
